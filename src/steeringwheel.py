8187f38dc87aecffeb5e8f75c197ebb890a44dfc
Stuart Bowman <stuartsoft2012@gmail.com>
2020-07-04 23:44:34 -0400

    Steering!

--- src/steeringwheel.py
+++ src/steeringwheel.py
@@ -1,3 +1,4 @@
+from constants import Constants
 from vehicledevice import VehicleDevice
 
 
@@ -5,6 +6,21 @@ class SteeringWheel(VehicleDevice):
 
     def __init__(self, servo):
         self.servo_ref = servo
+        self.servo_ref.value = Constants.SERVO_CENTER_PWM_VALUE
+
+    def set_heading(self, command):
+        magnitude = int(command[1:])
+        if (command[0] == 'L'):
+            servo_limit = 1
+        elif(command[0] == 'R'):
+            servo_limit = -1
+        else:
+            self.servo_ref.value = Constants.SERVO_CENTER_PWM_VALUE
+            return
+
+        servo_direction_range = servo_limit - Constants.SERVO_CENTER_PWM_VALUE
+        interpolated_value = ((magnitude)/100) * servo_direction_range + Constants.SERVO_CENTER_PWM_VALUE
+        self.servo_ref.value = interpolated_value
 
     def update(self):
         pass
--- test/steeringwheeltest.py
+++ test/steeringwheeltest.py
@@ -2,6 +2,7 @@ import unittest
 
 from gpiozero.pins.mock import MockFactory
 
+from constants import Constants
 from steeringwheel import SteeringWheel
 
 
@@ -10,10 +11,39 @@ class SteeringWheelTest(unittest.TestCase):
     SOME_PIN_ID = 13
 
     def setUp(self) -> None:
-        self.mock_servo = MockFactory(pin_class="mockpwmpin").pin(self.SOME_PIN_ID)
+        self.mock_servo = MockFactory(pin_class="mockpwmpin").pin(SteeringWheelTest.SOME_PIN_ID)
+        self.test_obj = SteeringWheel(self.mock_servo)
 
     def test_can_construct(self):
 
-        test_obj = SteeringWheel(self.mock_servo)
+        self.assertIs(self.test_obj.servo_ref, self.mock_servo)
+        self.assertEqual(Constants.SERVO_CENTER_PWM_VALUE, self.test_obj.servo_ref.value)
 
-        self.assertIs(test_obj.servo_ref, self.mock_servo)
+    def test_can_map_direction_to_servo_value(self):
+
+        self.test_obj.set_heading('N000')
+
+        self.assertEqual(Constants.SERVO_CENTER_PWM_VALUE, self.test_obj.servo_ref.value)
+
+        self.test_obj.set_heading('L100')
+
+        self.assertEqual(1, self.test_obj.servo_ref.value)
+
+        self.test_obj.set_heading('R100')
+
+        self.assertEqual(-1, self.test_obj.servo_ref.value)
+
+    def test_can_interpolate_values_based_on_configured_servo_center(self):
+
+        self.test_obj.set_heading('L050')
+        expected_value = (1 - Constants.SERVO_CENTER_PWM_VALUE)/2 + Constants.SERVO_CENTER_PWM_VALUE
+        self.assertEqual(expected_value, self.test_obj.servo_ref.value)
+
+        self.test_obj.set_heading('R025')
+        expected_value = (-1 - Constants.SERVO_CENTER_PWM_VALUE)/4 + Constants.SERVO_CENTER_PWM_VALUE
+        self.assertEqual(expected_value, self.test_obj.servo_ref.value)
+
+    def test_should_set_to_center_if_neutral(self):
+
+        self.test_obj.set_heading('N100')
+        self.assertEqual(Constants.SERVO_CENTER_PWM_VALUE, self.test_obj.servo_ref.value)
