584e85eb77b4bced2af0005a2f3ab973af4641df
Stuart Bowman <stuartsoft2012@gmail.com>
2020-07-05 12:23:10 -0400

    Adding steering wheel to our update list

--- src/rtrcmd.py
+++ src/rtrcmd.py
@@ -1,15 +1,19 @@
 # Rotor.ai Command Line ESC Interface
 # Copyright 2020 Robbie and Stu
-from gpiozero import PWMLED
+import time
+
+from gpiozero import PWMLED, Servo
 
 from constants import Constants
+from steeringwheel import SteeringWheel
 from vehiclelight import VehicleLight
 
 headlights = VehicleLight(PWMLED(Constants.GPIO_PIN_HEADLIGHTS))
 driverSideTailLight = VehicleLight(PWMLED(Constants.GPIO_PIN_LEFT_TAILLIGHT))
 passengerSideTailLight = VehicleLight(PWMLED(Constants.GPIO_PIN_RIGHT_TAILLIGHT))
+steeringWheel = SteeringWheel(Servo(Constants.GPIO_PIN_STEERING_SERVO))
 
-items = [headlights, driverSideTailLight, passengerSideTailLight]
+items = [headlights, driverSideTailLight, passengerSideTailLight, steeringWheel]
 
 headlights.set_bright()
 driverSideTailLight.start_blinking()
