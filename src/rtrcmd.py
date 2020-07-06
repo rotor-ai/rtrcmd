008b5d14c378ccb94153fa3cc32bd846fc99b12c
Stuart Bowman <stuartsoft2012@gmail.com>
2020-07-06 13:08:53 -0400

    Saving progress

--- src/rtrcmd.py
+++ src/rtrcmd.py
@@ -1,9 +1,11 @@
 # Rotor.ai Command Line ESC Interface
 # Copyright 2020 Robbie and Stu
+import time
 
 from gpiozero import PWMLED, Servo
 
 from constants import Constants
+from motor import Motor
 from steeringwheel import SteeringWheel
 from vehiclelight import VehicleLight
 
@@ -11,16 +13,27 @@ headlights = VehicleLight(PWMLED(Constants.GPIO_PIN_HEADLIGHTS))
 driverSideTailLight = VehicleLight(PWMLED(Constants.GPIO_PIN_LEFT_TAILLIGHT))
 passengerSideTailLight = VehicleLight(PWMLED(Constants.GPIO_PIN_RIGHT_TAILLIGHT))
 steeringWheel = SteeringWheel(Servo(Constants.GPIO_PIN_STEERING_SERVO))
+motor = Motor(Servo(Constants.GPIO_PIN_ELECTRONIC_SPEED_CONTROLLER))
 
-items = [headlights, driverSideTailLight, passengerSideTailLight, steeringWheel]
+items = [headlights, driverSideTailLight, passengerSideTailLight, steeringWheel, motor]
 
-headlights.set_bright()
-driverSideTailLight.start_blinking()
-passengerSideTailLight.start_blinking()
-
-
-while True:
-
-	for item in items:
-		item.update()
+headlights.set_off()
+driverSideTailLight.set_off()
+steeringWheel.set_heading('N000')
+motor.set_throttle('N000')
+time.sleep(1)
+steeringWheel.set_heading('L100')
+time.sleep(1)
+steeringWheel.set_heading('R100')
+time.sleep(1)
+steeringWheel.set_heading('N000')
+time.sleep(1)
+motor.set_throttle('F100')
+time.sleep(5)
+motor.servo_ref.setOff()
+motor.set_throttle('N000')
+time.sleep(5)
+motor.set_throttle('R100')
+time.sleep(5)
+motor.set_throttle('N000')
 
