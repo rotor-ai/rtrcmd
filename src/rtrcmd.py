7f8c188d9dd212b77dcaa876d5f5c13179d8469b
Stuart Bowman <stuartsoft2012@gmail.com>
2020-07-22 21:37:29 -0400

    It freaking does fwd and reverse correctly! Also, using the pigpio pin factory, which appears to have solved the 'hitching' problem

--- src/rtrcmd.py
+++ src/rtrcmd.py
@@ -1,12 +1,14 @@
 # Rotor.ai Command Line ESC Interface
 # Copyright 2020 Robbie and Stu
 import time
+from signal import pause
 
-from gpiozero import PWMLED, Servo, Device
+from gpiozero import PWMLED, Servo, LED
 from gpiozero.pins.rpigpio import RPiGPIOFactory
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero.pins.native import NativeFactory
 
+pigpiofactory = PiGPIOFactory('localhost', 8888)
 
 from constants import Constants
 from motor import Motor
@@ -14,59 +16,65 @@ from steeringwheel import SteeringWheel
 from vehiclelight import VehicleLight
 
 motorPeriod_ms = 20   #unit in milliseconds
-minDuty_pct = 5    #unit in percentage
-maxDuty_pct = 10   #unit in percentage
 
-# head_lights = VehicleLight(PWMLED(Constants.GPIO_PIN_HEADLIGHTS))
-# fog_lights = VehicleLight(PWMLED(Constants.GPIO_PIN_FOGLIGHTS))
-# turn_signal_left = VehicleLight(PWMLED(Constants.GPIO_PIN_LEFT_TURNSIGNAL))
-# turn_signal_right = VehicleLight(PWMLED(Constants.GPIO_PIN_RIGHT_TURNSIGNAL))
-# reverse_lights = VehicleLight(PWMLED(Constants.GPIO_PIN_REVERSE_LIGHTS))
-# tail_light_left = VehicleLight(PWMLED(Constants.GPIO_PIN_LEFT_TAILLIGHT))
-# tail_light_right = VehicleLight(PWMLED(Constants.GPIO_PIN_RIGHT_TAILLIGHT))
+head_lights = VehicleLight(PWMLED(Constants.GPIO_PIN_HEADLIGHTS))
+fog_lights = VehicleLight(PWMLED(Constants.GPIO_PIN_FOGLIGHTS))
+turn_signal_left = VehicleLight(PWMLED(Constants.GPIO_PIN_LEFT_TURNSIGNAL))
+turn_signal_right = VehicleLight(PWMLED(Constants.GPIO_PIN_RIGHT_TURNSIGNAL))
+reverse_lights = VehicleLight(PWMLED(Constants.GPIO_PIN_REVERSE_LIGHTS))
+tail_light_left = VehicleLight(PWMLED(Constants.GPIO_PIN_LEFT_TAILLIGHT))
+tail_light_right = VehicleLight(PWMLED(Constants.GPIO_PIN_RIGHT_TAILLIGHT))
 
-# steeringWheel = SteeringWheel(Servo(Constants.GPIO_PIN_STEERING_SERVO))
+
+steeringWheel = SteeringWheel(Servo(Constants.GPIO_PIN_STEERING_SERVO))
 min_pulse = 1/1000
 max_pulse = 2/1000
-motor = Motor(Servo(Constants.GPIO_PIN_ELECTRONIC_SPEED_CONTROLLER, min_pulse_width=min_pulse, max_pulse_width=max_pulse , frame_width=motorPeriod_ms/1000))
+motor = Motor(Servo(Constants.GPIO_PIN_ELECTRONIC_SPEED_CONTROLLER, min_pulse_width=min_pulse, max_pulse_width=max_pulse , frame_width=motorPeriod_ms/1000, pin_factory=pigpiofactory))
 
-# lights = [head_lights, fog_lights,turn_signal_left, turn_signal_right, reverse_lights, tail_light_left, tail_light_right]
+lights = [head_lights, fog_lights,turn_signal_left, turn_signal_right, reverse_lights, tail_light_left, tail_light_right]
 
-throttle_delay = 0.1
-throttle_increment = 1
-print("waiting for ESC to calibrate...")
+print("Giving time for the ESC to calibrate...")
 time.sleep(4)
+
+throttle_delay = 100000000
+
+nextIncrementTime = time.time_ns() + throttle_delay
+currentValue = 0
+waxwane = 1
+
 
 while True:
 
-    for i in range(0, 100, throttle_increment):
-        cmd = 'F'+ str(format(i, '0>3'))
-        print(cmd)
+    if (time.time_ns() > nextIncrementTime):
+        currentValue += 1 * waxwane
+
+        if (currentValue > 50):
+            currentValue = 50
+            waxwane *= -1
+        if (currentValue < -50):
+            currentValue = -50
+            waxwane *= -1
+
+        if (currentValue == 0):
+            print("detaching...")
+            motor.servo_ref.detach()
+            time.sleep(1)
+            print("reattaching...")
+            motor.set_throttle('N000')
+            time.sleep(1)
+            if (waxwane == -1):
+                print("fake reverse for a moment...")
+                #https://www.youtube.com/watch?v=FzbJ8yVIm_c
+                #Thank you  GrandadIsAnOldMan!
+                motor.set_throttle('R020')
+                time.sleep(0.04)
+                motor.set_throttle('N000')
+        # if abs(currentValue) < 40:
+        #     cmd = 'N000'
+        elif currentValue < 0:
+            cmd = 'R' + str(format(abs(currentValue), '0>3'))
+        else:
+            cmd = 'F' + str(format(abs(currentValue), '0>3'))
         motor.set_throttle(cmd)
-        time.sleep(throttle_delay)
-
-    # pause()
-
-    for i in range(100, 0, -1*throttle_increment):
-        cmd = 'F'+ str(format(i, '0>3'))
-        print(cmd)
-        motor.set_throttle(cmd)
-        time.sleep(throttle_delay)
-
-    motor.set_throttle('N000')
-    time.sleep(2)
-
-    for i in range(0, 101, throttle_increment):
-        cmd = 'R'+ str(format(i, '0>3'))
-        print(cmd)
-        motor.set_throttle(cmd)
-        time.sleep(throttle_delay)
-
-    for i in range(100, 0, -1*throttle_increment):
-        cmd = 'R'+ str(format(i, '0>3'))
-        print(cmd)
-        motor.set_throttle(cmd)
-        time.sleep(throttle_delay)
-
-    motor.set_throttle('N000')
-    time.sleep(2)
+        print(cmd + ' ' + str(motor.servo_ref.value))
+        nextIncrementTime = time.time_ns() + throttle_delay
