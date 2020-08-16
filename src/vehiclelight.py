305a6eaa2cd1cab8208290f23a591cf4f8bcc746
Stuart Bowman <stuartsoft2012@gmail.com>
2020-08-15 21:04:26 -0400

    Adding logging to vehicleLight

--- src/vehiclelight.py
+++ src/vehiclelight.py
@@ -5,7 +5,9 @@ from vehicledevice import VehicleDevice
 
 class VehicleLight(VehicleDevice):
 
-    def __init__(self, ledRef):
+
+    def __init__(self, logger, ledRef) -> None:
+        super().__init__(logger)
         self.ledRef = ledRef
         self.ledRef.value = 0
         self.blinking = False
@@ -14,19 +16,23 @@ class VehicleLight(VehicleDevice):
     def set_off(self):
         self.ledRef.value = Constants.LIGHT_OFF_PWM_VAL
         self.blinking = False
+        self.logger.info("Turning off vehicleLight")
 
     def set_dim(self):
         self.ledRef.value = Constants.LIGHT_DIM_PWM_VAL
         self.blinking = False
+        self.logger.info("Setting vehicleLight to dim")
 
     def set_bright(self):
         self.ledRef.value = Constants.LIGHT_BRIGHT_PWM_VAL
         self.blinking = False
+        self.logger.info("Setting vehicleLight to full brightness")
 
     def start_blinking(self):
         self.blinking = True
         self.nextBlinkTime = self.now_wrapper() + Constants.LIGHT_BLINK_DURATION_NS
         self.ledRef.value = Constants.LIGHT_BRIGHT_PWM_VAL
+        self.logger.info("Started blinking vehicleLight")
 
     # THIS METHOD IS NOT UNDER TEST
     def now_wrapper(self):
--- test/vehiclelighttest.py
+++ test/vehiclelighttest.py
@@ -1,3 +1,4 @@
+import logging
 import unittest
 
 from gpiozero.pins.mock import MockFactory
@@ -8,11 +9,16 @@ from vehiclelight import VehicleLight
 class VehicleLightTest(unittest.TestCase):
     SOME_PIN_ID = 12
 
+    def assertOnlyLog(self, logs, expectedMsg, expectedLevel = "INFO"):
+        self.assertEqual(1, len(logs.records))
+        self.assertEqual(expectedLevel, logs.records[0].levelname)
+        self.assertEqual(expectedMsg, logs.records[0].message)
+
     def setUp(self) -> None:
         self.mock_pwmLed = MockFactory(pin_class="mockpwmpin").pin(self.SOME_PIN_ID)
         self.mock_pwmLed.value = 0.5
-
-        self.test_obj = VehicleLight(self.mock_pwmLed)
+        self.logger = logging.Logger("some name")
+        self.test_obj = VehicleLight(self.logger, self.mock_pwmLed)
 
     def test_should_construct(self):
         self.assertIs(self.mock_pwmLed, self.test_obj.ledRef)
@@ -24,37 +30,45 @@ class VehicleLightTest(unittest.TestCase):
         self.test_obj.blinking = True
         self.test_obj.ledRef.value = 999
 
-        self.test_obj.set_off()
+        with self.assertLogs(self.logger) as logs:
+            self.test_obj.set_off()
 
         self.assertEqual(0, self.test_obj.ledRef.value)
         self.assertFalse(self.test_obj.blinking)
+        self.assertOnlyLog(logs, "Turning off vehicleLight")
 
     def test_set_dim(self):
         self.test_obj.blinking = True
         self.test_obj.ledRef.value = 999
 
-        self.test_obj.set_dim()
+        with self.assertLogs(self.logger) as logs:
+            self.test_obj.set_dim()
 
         self.assertEqual(Constants.LIGHT_DIM_PWM_VAL, self.test_obj.ledRef.value)
         self.assertFalse(self.test_obj.blinking)
+        self.assertOnlyLog(logs, "Setting vehicleLight to dim")
 
     def test_set_bright(self):
         self.test_obj.blinking = True
         self.test_obj.ledRef.value = 999
 
-        self.test_obj.set_bright()
+        with self.assertLogs(self.logger) as logs:
+            self.test_obj.set_bright()
 
         self.assertEqual(Constants.LIGHT_BRIGHT_PWM_VAL, self.test_obj.ledRef.value)
         self.assertFalse(self.test_obj.blinking)
+        self.assertOnlyLog(logs, "Setting vehicleLight to full brightness")
 
     def test_should_start_blinking(self):
         self.test_obj.now_wrapper = lambda: 100000000
 
-        self.test_obj.start_blinking()
+        with self.assertLogs(self.logger) as logs:
+            self.test_obj.start_blinking()
 
         self.assertEqual(True, self.test_obj.blinking)
         self.assertEqual(600000000, self.test_obj.nextBlinkTime)
         self.assertEqual(Constants.LIGHT_BRIGHT_PWM_VAL, self.test_obj.ledRef.value)
+        self.assertOnlyLog(logs, "Started blinking vehicleLight")
 
     def test_should_update_blinker(self):
         self.test_obj.now_wrapper = lambda: 100000000
