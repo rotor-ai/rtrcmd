227dc5556e2f92606cf80654bf4ead717c24df9a
Stuart Bowman <stuartsoft2012@gmail.com>
2021-03-13 14:54:26 -0500

    Text is scrolling smoothly on the digital display

--- src/vehicle/segmented_display_behaviors.py
+++ src/vehicle/segmented_display_behaviors.py
@@ -1,11 +1,27 @@
 
 def address_display(display, network_adapter):
-    ipaddress = "    "
     if len(network_adapter)>2 and len(network_adapter[2]) > 0 and network_adapter[2][0].__contains__('addr'):
-        ipaddress += network_adapter[2][0]['addr']
+        ipaddress = network_adapter[2][0]['addr']
     else:
-        ipaddress += "127.0.0.1"
+        ipaddress = "127.0.0.1"
 
-    position = int(display.run_duration() / display.loop_delay()) #% len(ipaddress)
-    scrolling_ip_address = ipaddress[position:] + ipaddress[:position]
-    display.set_text(scrolling_ip_address)
+    #group decimals with preceeding character
+    digitDecimalPairs = [(' ',''),(' ',''),(' ',''),(' ','')]
+    while len(ipaddress) > 0:
+        digit = ipaddress[0]
+        ipaddress = ipaddress[1:]
+        decimal = ''
+        if len(ipaddress) > 0 and ipaddress[0] == '.':
+            decimal = ipaddress[0]
+            ipaddress = ipaddress[1:]
+
+        digitDecimalPairs.append((digit, decimal))
+
+    position = int(display.run_duration() / display.loop_delay()) % len(digitDecimalPairs)
+
+    textToDisplay = ''
+    for dp in (digitDecimalPairs[position:] + digitDecimalPairs[:position])[:4]:
+        textToDisplay += dp[0]
+        textToDisplay += dp[1]
+
+    display.set_text(textToDisplay)
