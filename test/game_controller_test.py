f787a65ef3784198fe1a65eb0863a44dec3c085a
Stuart Bowman <stuartsoft2012@gmail.com>
2021-04-14 00:47:49 -0400

    Adding new from_json() method to our GameControllerCalibration object

--- test/game_controller_test.py
+++ test/game_controller_test.py
@@ -27,3 +27,49 @@ class GameControllerCalibrationTest(unittest.TestCase):
 
         self.assertDictEqual(json, expectedJson)
 
+    def test_from_json_should_populate_all_fields(self):
+
+        inputJson = dict()
+        inputJson['left_trigger_max'] = 4
+        inputJson['right_trigger_max'] = 5
+        inputJson['joystick_boundary'] = 6
+        subject = GameControllerCalibration()
+
+        subject.from_json(inputJson)
+
+        self.assertEqual(4, subject.left_trigger_max)
+        self.assertEqual(5, subject.right_trigger_max)
+        self.assertEqual(6, subject.joystick_boundary)
+
+    def test_from_json_should_handle_all_missing_keys(self):
+
+        subject = GameControllerCalibration()
+        subject.left_trigger_max = 1
+        subject.right_trigger_max = 2
+        subject.joystick_boundary = 3
+        inputJson = dict()
+
+        subject.from_json(inputJson)
+
+        self.assertEqual(1, subject.left_trigger_max)
+        self.assertEqual(2, subject.right_trigger_max)
+        self.assertEqual(3, subject.joystick_boundary)
+
+    def test_from_json_should_handle_individual_missing_keys(self):
+
+        subject = GameControllerCalibration()
+        subject.left_trigger_max = 1
+        subject.right_trigger_max = 2
+        subject.joystick_boundary = 3
+        inputJson = dict()
+        inputJson['left_trigger_max'] = 11
+
+        subject.from_json(inputJson)
+
+        self.assertEqual(11, subject.left_trigger_max)
+
+        inputJson['right_trigger_max'] = 22
+        subject.from_json(inputJson)
+
+        self.assertEqual(22, subject.right_trigger_max)
+
